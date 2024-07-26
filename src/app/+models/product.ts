6e965a5040bb5dc9a8c7a02f0f9a477a18f2c528
Amir <amirsiahvashi25@gmail.com>
2024-07-26 23:20:20 +0330

    v1.5

---
+++ src/app/+models/product.ts
@@ -0,0 +1,7 @@
+export interface Product{
+    title:string;
+    img:string;
+    count:number;
+    price:number;
+    details:string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './+components/products/products.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    ProductsComponent,
+    ProductsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Project3';
+
 }
